161cfbeb6bbc515f0248955a5705ad0d5dcdce50
Jorge Salas <cdmx2767@amerike.edu.mx>
2025-05-08 11:07:24 -0600

    cambios

--- InformaticaGraficaHDRP/Assets/Carta/LightDirection.hlsl
+++ InformaticaGraficaHDRP/Assets/Carta/LightDirection.hlsl
@@ -1,13 +1,18 @@
+
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+
+
 void LightDirection_half(out half3 Direction, out half DistanceAtten, out half3 LightColor)
 {
   #if SHADERGRAPH_PREVIEW
-      Direction = half3(0, 1, 0);
-      DistanceAtten = 1;
-      LightColor = half3(1, 1, 1);
+    Direction = half3(0,1,0);
+    DistanceAtten = 1;
+    LightColor = half3(1,1,1);
   #else
-      Light light = GetMainLight();
-      Direction = normalize(light.direction);
-      DistanceAtten = light.distanceAttenuation;
-      LightColor = light.color;
+    Light light = GetMainLight();
+    Direction = normalize(light.direction);
+    DistanceAtten = light.distanceAttenuation;
+    LightColor = light.color;
   #endif
 }
+
